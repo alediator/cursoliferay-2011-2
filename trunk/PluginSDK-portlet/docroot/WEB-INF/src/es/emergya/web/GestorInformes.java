3b3f433b0213f0e9c7a70a3a8a87d17cad0cbec3
Alejandro Díaz Torres <adiaz@emergya.com>
2011-09-21 17:51:19 +0200

    New Portlet (wizard)

---
+++ trunk/PluginSDK-portlet/docroot/WEB-INF/src/es/emergya/web/GestorInformes.java
@@ -0,0 +1,11 @@
+package es.emergya.web;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class GestorInformes
+ */
+public class GestorInformes extends MVCPortlet {
+ 
+
+}
