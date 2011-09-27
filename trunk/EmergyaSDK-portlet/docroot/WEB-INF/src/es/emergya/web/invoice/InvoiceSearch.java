cf702a7484c233115b679ae8892c4c4ce7dbc5e3
Alejandro Díaz Torres <adiaz@emergya.com>
2011-09-27 17:34:17 +0200

    Constructor completo en InvoiceSearch

--- trunk/EmergyaSDK-portlet/docroot/WEB-INF/src/es/emergya/web/invoice/InvoiceSearch.java
+++ trunk/EmergyaSDK-portlet/docroot/WEB-INF/src/es/emergya/web/invoice/InvoiceSearch.java
@@ -9,6 +9,12 @@ import javax.portlet.PortletRequest;
 import javax.portlet.PortletURL;
 
 import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portlet.PortalPreferences;
+import com.liferay.portlet.PortletPreferencesFactoryUtil;
 
 import es.emergya.negocio.model.Factura;
 
@@ -23,18 +29,63 @@ public class InvoiceSearch extends SearchContainer<Factura>{
 				new InvoiceSearchTerm(portletRequest), DEFAULT_CUR_PARAM,
 				DEFAULT_CUR, 5, iteratorURL, headerNames, EMPTY_RESULTS_MESSAGE);
 		
-		// Paso 1.2: Inicializacion de iteratorURL en funcion de los parametros del fltro
-		InvoiceDisplayTerms displayTerms = (InvoiceDisplayTerms) getDisplayTerms();
-		iteratorURL.setParameter(InvoiceDisplayTerms.DESCRIPCION, displayTerms.getDescripcion());
-		iteratorURL.setParameter(InvoiceDisplayTerms.NOMBRE_CLIENTE, displayTerms.getNombreCliente());
-		iteratorURL
-				.setParameter(
-						InvoiceDisplayTerms.IMPORTE_TOTAL,
-						((Double) displayTerms.getImporteTotal() != null) ? ((Double) displayTerms
-								.getImporteTotal()).toString() : "");
+		try {
 		
-		//Paso 1.3: Inicializacion de los ordenable headers
-		setOrderableHeaders(orderableHeaders);
+			// Paso 1.2: Inicializacion de iteratorURL en funcion de los parametros del fltro
+			InvoiceDisplayTerms displayTerms = (InvoiceDisplayTerms) getDisplayTerms();
+			iteratorURL.setParameter(InvoiceDisplayTerms.DESCRIPCION, displayTerms.getDescripcion());
+			iteratorURL.setParameter(InvoiceDisplayTerms.NOMBRE_CLIENTE, displayTerms.getNombreCliente());
+			iteratorURL
+					.setParameter(
+							InvoiceDisplayTerms.IMPORTE_TOTAL,
+							((Double) displayTerms.getImporteTotal() != null) ? ((Double) displayTerms
+									.getImporteTotal()).toString() : "");
+			
+			// Paso 1.3: Inicializacion de los ordenable headers
+			setOrderableHeaders(orderableHeaders);
+			
+			// Paso 1.4: Definir los criterios de ordenacion
+			String orderByCol = ParamUtil.getString(portletRequest, "orderByCol");
+			String orderByType = ParamUtil.getString(portletRequest, "orderByType");
+			
+			PortalPreferences preferences = PortletPreferencesFactoryUtil.getPortalPreferences(portletRequest);
+			
+			// Guarda y establece los criterios de ordenacion por defecto
+			if (Validator.isNotNull(orderByCol)
+					&& Validator.isNotNull(orderByType)) {
+				preferences.setValue("facturasByCol", "facturas-order-by-col",
+						orderByCol);
+				preferences.setValue("facturasByType",
+						"facturas-order-by-type", orderByType);
+			} else {
+				orderByCol = preferences.getValue("facturasByCol",
+						"facturas-order-by-col",
+						InvoiceDisplayTerms.DESCRIPCION);
+				orderByType = preferences.getValue("facturasByType",
+						"facturas-order-by-col", "asc");
+			}
+			
+			setOrderByCol(orderByCol);
+			setOrderByType(orderByType);
+			
+			boolean asc = true;
+			if(orderByType.equals("desc")){
+				asc = false;
+			}
+
+			// OrderByComparator
+			OrderByComparator orderByComparator = null;
+			if(orderByCol.equals(InvoiceDisplayTerms.DESCRIPCION)){
+				orderByComparator = new DescripcionInvoiceComparator(asc);
+			}else{
+				orderByComparator = new NombreClienteInvoiceComparator(asc);
+			}
+			setOrderByComparator(orderByComparator);
+			
+		} catch (SystemException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	
