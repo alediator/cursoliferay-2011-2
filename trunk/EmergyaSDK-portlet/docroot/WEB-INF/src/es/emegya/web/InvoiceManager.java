61ffb1b3adbfb742dbdd755f5894a961e14956a2
Alejandro Díaz Torres <adiaz@emergya.com>
2011-09-27 17:39:21 +0200

    InvoiceManager portlet con wizard

---
+++ trunk/EmergyaSDK-portlet/docroot/WEB-INF/src/es/emegya/web/InvoiceManager.java
@@ -0,0 +1,11 @@
+package es.emegya.web;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class InvoiceManager
+ */
+public class InvoiceManager extends MVCPortlet {
+ 
+
+}
